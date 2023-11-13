3d2146ef376495086a66c17fb025f5f7567bad33
mcleanojames9 <mcleanojames9@gmail.com>
2023-11-13 12:36:57 -0500

    setting up http request

--- ng-building-repair-est/src/app/services/data-transfer.service.ts
+++ ng-building-repair-est/src/app/services/data-transfer.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,8 @@ export class DataTransferService {
   constructor(private http: HttpClient) {}
     private headers = new HttpHeaders({'Content-Type':'application/json'});
     url='http://localhost:5000'
+
+  getUser(user:User): Observable<User>{
+    return this.http.get<User>(this.url+'/login');
+  }
 }
